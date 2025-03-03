95f0042f2986a6a47620a55e7cd6cdefcf34245a
Thom de Wit <tj.dewit2@student.avans.nl>
2025-03-03 14:15:23 +0100

    W3O2 to fix last bit

---
+++ W3/Opd2/Opd2/main.c
@@ -0,0 +1,49 @@
+/*
+ * Opd2.c
+ *
+ * Created: 28-2-2025 15:06:07
+ * Author : Kersjan14
+ */ 
+
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// Timer2 interrupt handler
+ISR(TIMER2_COMP_vect) {
+	static uint8_t state = 0;
+	
+	switch(state) {
+		case 0: // Hoog voor 15ms
+		PORTD |= (1 << PD7);  // Zet PD7 hoog
+		OCR2 = 117;           // Reset timer voor 15ms
+		state = 1;
+		break;
+		
+		case 1: // Laag voor 25ms
+		PORTD &= ~(1 << PD7); // Zet PD7 laag
+		OCR2 = 195;           // Reset timer voor 25ms
+		state = 0;
+		break;
+	}
+}
+
+int main(void) {
+	// Configureer PORTD.7 als output
+	DDRD |= (1 << PD7);
+	
+	// Timer2 configuratie
+	TCCR2 = (1<<WGM21) | (1<<CS22) | (1<<CS20);  // CTC mode, prescaler 1024
+	OCR2 = 117;  // Begin met 15ms interval
+	TIMSK |= (1<<OCIE2);  // Enable Timer2 compare interrupt
+	sei();  // Enable global interrupts
+	
+	while(1) {
+		// Programma blijft hier hangen
+		// Timer2 interrupt handelt de pulsen
+	}
+	
+	return 0;
+}
+
